cf443d2855014b94ca2e1337a5ca09b244c1fc63
Qin_Shihuang <cafeboss@live.cn>
2016-08-16 00:41:31 +0800

    Improve color

---
+++ app/pages/roll-detail-page/roll-detail-page.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the RollDetailPagePage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/roll-detail-page/roll-detail-page.html',
+})
+export class RollDetailPagePage {
+
+  constructor(private nav: NavController) {
+
+  }
+
+}
---
+++ app/pages/task-detail-page/task-detail-page.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the TaskDetailPagePage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/task-detail-page/task-detail-page.html',
+})
+export class TaskDetailPagePage {
+
+  constructor(private nav: NavController) {
+
+  }
+
+}
